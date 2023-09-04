d3486a36dc2fa8ca2568fb51f8f2f947e314cf71
Pedro-Henrique-da-Silva-Ferreira <141925740+Pedro-Henrique-da-Silva-Ferreira@users.noreply.github.com>
2023-09-04 09:55:58 -0300

    Update Fachada.java

--- blocoDeNotas/src/Fachada.java
+++ blocoDeNotas/src/Fachada.java
@@ -1,28 +1,28 @@
-vimport java.util.ArrayList;
+
 import java.util.List;
 
 public class Fachada {
 
     private BlocoDeNotas bloco = new BlocoDeNotas();
 
-    public BlocoDeNotas adicionar(String texto){
+    public Anotacao adicionar(String texto){
         return bloco.adicionar(texto);
     }
 
-    public BlocoDeNotas remover(int id) {
+    public Anotacao remover(int id) {
         return bloco.remover(id);
     }
 
-    public BlocoDeNotas editar(int id, String texto) {
+    public Anotacao editar(int id, String texto) {
         return bloco.editar(id, texto);
     }
 
-    public BlocoDeNotas recuperar(int id) {
+    public boolean recuperar(int id) {
         return bloco.recuperar(id);
     }
 
-    public BlocoDeNotas pesquisa(String padrao) {
-        return bloco;
+    public String pesquisa(String padrao) {
+        return bloco.pesquisa(padrao);
     }
     public String mostrar (){
         return bloco.mostrar();
