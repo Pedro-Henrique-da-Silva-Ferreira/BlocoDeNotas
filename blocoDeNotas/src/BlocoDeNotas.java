8efbacbef03c1e02d3873c1e0487c1f3a5d67c27
Pedro-Henrique-da-Silva-Ferreira <141925740+Pedro-Henrique-da-Silva-Ferreira@users.noreply.github.com>
2023-09-04 09:52:07 -0300

    Update BlocoDeNotas.java

--- blocoDeNotas/src/BlocoDeNotas.java
+++ blocoDeNotas/src/BlocoDeNotas.java
@@ -1,51 +1,57 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class BlocoDeNotas  {
+public class BlocoDeNotas {
 
     private List<Anotacao> anotacoes = new ArrayList<>();
 
-    public Anotacao adicionar(String texto){
+    public Anotacao adicionar(String texto) {
 
         Anotacao anotacao = new Anotacao(texto, anotacoes.size() + 1);
         anotacoes.add(anotacao);
         return anotacao;
     }
 
-    public Anotacao remover(int id){
-        Anotacao anotacao = anotacoes.get(id);
-        anotacao.remover(id);
+    public Anotacao remover(int id) {
+        Anotacao anotacao = anotacoes.get(id - 1);
+        anotacao.setRemovido(id);
         return anotacao;
 
     }
 
-    public Anotacao editar(int id,String texto){
-        Anotacao anotacao = anotacoes.get(id+1);
+    public Anotacao editar(int id, String texto) {
+        Anotacao anotacao = anotacoes.get(id - 1);
         anotacao.setTexto(texto);
         return anotacao;
 
     }
 
-    public Anotacao recuperar(int id){
-        return anotacoes.get(id);
-
+    public boolean recuperar(int id) {
+        Anotacao anotacao = anotacoes.get(id - 1);
+        return anotacao.setRecuperar(id);
     }
 
-    public void pesquisa(String padrao){
-        for(Anotacao a : anotacoes){
+
+    public String pesquisa(String padrao) {
+        StringBuilder p = new StringBuilder();
+        for (Anotacao a : anotacoes) {
             if (a.toString().contains(padrao)) {
-                System.out.println(a.toString());
+                p.append(a);
             }
         }
+        return p.toString();
     }
 
-    public String mostrar (){
+    public String mostrar() {
         String s = "";
-        for (Anotacao a: anotacoes) {
-            s += a.toString();
+        for (Anotacao a : anotacoes) {
+            if (a.isRemovido() == false) {
+                s += a.toString();
+            }
+
         }
         return s;
     }
-
 
 }
