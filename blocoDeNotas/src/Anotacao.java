eca3d656313e8d8ca066aa7b7337952b3081f2d8
Pedro-Henrique-da-Silva-Ferreira <141925740+Pedro-Henrique-da-Silva-Ferreira@users.noreply.github.com>
2023-09-04 09:56:39 -0300

    Update Anotacao.java

--- blocoDeNotas/src/Anotacao.java
+++ blocoDeNotas/src/Anotacao.java
@@ -1,6 +1,6 @@
 import java.time.LocalDate;
 
-public class Anotacao extends BlocoDeNotas {
+public class Anotacao{
 
     private String texto;
     private LocalDate data;
@@ -33,55 +33,22 @@ public class Anotacao extends BlocoDeNotas {
     }
 
     public boolean isRemovido() {
+        return removido;
+    }
+
+    public boolean setRemovido(int id) {
         removido = true;
         return removido;
     }
 
-
-    public String toString() {
-        return "\ntexto: " + "|" +getTexto() + "|" +" data " + getData() + "\n";
-    }
-}import java.time.LocalDate;
-
-public class Anotacao extends BlocoDeNotas {
-
-    private String texto;
-    private LocalDate data;
-    private int id;
-    private boolean removido = false;
-
-    public Anotacao(String texto, int id) {
-
-        this.texto = texto;
-        this.id = id;
-        this.data = LocalDate.now();
-    }
-
-
-    public String getTexto() {
-        return texto;
-    }
-
-    public void setTexto(String texto) {
-        this.texto = texto;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public LocalDate getData() {
-
-        return data;
-    }
-
-    public boolean isRemovido() {
-        removido = true;
-        return removido;
+    public boolean setRecuperar(int id){
+        return removido = false;
     }
 
 
     public String toString() {
-        return "\ntexto: " + "|" +getTexto() + "|" +" data " + getData() + "\n";
+        return "texto: " + "|" +getTexto() + "|" +" data " + getData() + " Id:" + getId()
+                + " " +  isRemovido() + "\n";
     }
 }
+
